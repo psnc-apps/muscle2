a3f34cc95bf13225388a2d8b6564cfda1f4ce509
Mariusz Mamonski <mamonski@man.poznan.pl>
2011-07-28 13:29:00 +0000

    ConnectionSchema from the newest version
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@37 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/java/muscle/core/ConnectionScheme.java
+++ src/java/muscle/core/ConnectionScheme.java
@@ -21,33 +21,30 @@ This file is part of MUSCLE (Multiscale Coupling Library and Environment).
 
 package muscle.core;
 
-import java.util.logging.Logger;
+import jade.core.AID;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
 import java.io.StringWriter;
+import java.net.URI;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
+import java.util.logging.Logger;
 
+import muscle.Constant;
 import muscle.exception.MUSCLERuntimeException;
-
-import muscle.gui.graph.jung.ConnectionSchemeJUNGPanel;
+import muscle.gui.graph.ConnectionSchemeViewable;
 import muscle.gui.graph.Edge;
 import muscle.gui.graph.Vertex;
-import jade.core.AID;
-
-import utilities.MiscTool;
-
-import muscle.gui.graph.ConnectionSchemeViewable;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import javax.swing.JFrame;
-import java.util.UUID;
-import muscle.Constant;
-import java.io.File;
-import java.net.URI;
+import muscle.gui.graph.jung.ConnectionSchemeJUNGPanel;
 import utilities.Env;
+import utilities.MiscTool;
 
 /**
 describes the p2p connections between kernels
@@ -55,25 +52,29 @@ describes the p2p connections between kernels
 */
 public class ConnectionScheme implements Serializable {
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 	private LinkedList<ExitDescription> targetExitDescriptions = new LinkedList<ExitDescription>(); // leafs of a connection chain entrance->conduit->exit
 	private transient Logger logger = muscle.logging.Logger.getLogger(ConnectionScheme.class);
 	protected Env env;
 	private String cs_file_uri = "cs_file_uri";
-	
+
 	{
-//		env = loadEnv();		
-		env = CxADescription.ONLY.subenv(this.getClass());
+//		env = loadEnv();
+		this.env = CxADescription.ONLY.subenv(this.getClass());
 	}
-	
+
 //	static protected utilities.Env loadEnv() {
 //		return CxADescription.ONLY.subenv(this.getClass());
 //	}
 
 	public ConnectionScheme() {
-	
-		init();
+
+		this.init();
 	}
-	
+
 	/**
 	ConnectionScheme input consists of a String wherein each line describes a possible communication chain (backwards notation: exit<-conduit<-entrance).
 	Every line consists of three parts:<br>
@@ -92,7 +93,7 @@ public class ConnectionScheme implements Serializable {
 
 		String connectionSchemeText = null;
 		try {
-			connectionSchemeText =  MiscTool.fileToString(new File(new URI((String)env.get(cs_file_uri))), Constant.Text.COMMENT_INDICATOR);
+			connectionSchemeText =  MiscTool.fileToString(new File(new URI((String)this.env.get(this.cs_file_uri))), Constant.Text.COMMENT_INDICATOR);
 		}
 		catch(java.net.URISyntaxException e) {
 			throw new RuntimeException(e);
@@ -106,7 +107,7 @@ public class ConnectionScheme implements Serializable {
 		for(String line : lines) {
 			String[] items = line.split("( +?)|(\t+?)");
 			if(items.length != 3) {
-				logger.severe("connection scheme has unknown format");
+				this.logger.severe("connection scheme has unknown format");
 				return;
 			}
 
@@ -117,34 +118,36 @@ public class ConnectionScheme implements Serializable {
 			assert items[1].indexOf('#') != (items[1].indexOf('(')-1) : "Error: empty id, omit # for an empty id";
 			assert items[1].indexOf('#') != (items[1].length()-1) : "Error: empty id, omit # for an empty id";
 			assert items[1].indexOf('(') != (items[1].indexOf(')')-1) : "Error: empty (), omit the '()' for an empty arg list";
-			
+
 			String conduitClassName = parts[0];
 			String conduitID = null;
 			String[] conduitArgs = null;
 
 			if(parts.length > 1) {
-				String argsText = "";
-				if(items[1].indexOf('#') > -1)
+				if(items[1].indexOf('#') > -1) {
 					conduitID = parts[1];
+				}
 				if(items[1].indexOf('(') > -1 ) {
 					assert items[1].indexOf(')') > -1;
 					conduitArgs = parts[parts.length-1].split(","); // last are the args, if any (parts[1] or parts[2])
 				}
 			}
-			
-			if(conduitID == null)
+
+			if(conduitID == null) {
 				conduitID = UUID.randomUUID().toString();
-			if(conduitArgs == null)
+			}
+			if(conduitArgs == null) {
 				conduitArgs = new String[0];
-				
-			addConnection(items[2], conduitClassName, conduitID, conduitArgs, items[0]);
+			}
+
+			this.addConnection(items[2], conduitClassName, conduitID, conduitArgs, items[0]);
 		}
 	}
 
-	
+
 	//
 	public boolean isComplete() {
-	
+
 		return true;
 	}
 
@@ -155,25 +158,27 @@ public class ConnectionScheme implements Serializable {
 	public Pipeline addConnection(String entranceID, String conduitClassName, String conduitID, String[] conduitArgs, String exitID) {
 
 		// see if this entrance is already added, otherwise create new description for it
-		EntranceDescription entrance = entranceDescriptionForID(entranceID);
-		if( entrance == null )
+		EntranceDescription entrance = this.entranceDescriptionForID(entranceID);
+		if( entrance == null ) {
 			entrance = new EntranceDescription(entranceID);
+		}
 
 		// see if this conduit is already added, otherwise create new description for it
-		ConduitDescription conduit = conduitDescriptionForNameIDEntrance(conduitClassName, conduitID);
-		if( conduit == null )
+		ConduitDescription conduit = this.conduitDescriptionForNameIDEntrance(conduitClassName, conduitID);
+		if( conduit == null ) {
 			conduit = new ConduitDescription(conduitClassName, conduitID, conduitArgs, entrance);
-		else {
-			if(!conduit.getEntranceDescription().getID().equals(entrance.getID()))
+		} else {
+			if(!conduit.getEntranceDescription().getID().equals(entrance.getID())) {
 				throw new MUSCLERuntimeException("Error: can not feed conduit <"+conduit.toString()+"> from different entrances");
+			}
 		}
 
 
 		// see if this exit is already there
-		ExitDescription exit = exitDescriptionForID(exitID);
+		ExitDescription exit = this.exitDescriptionForID(exitID);
 		if(exit == null) {
 			exit = new ExitDescription(exitID, conduit);
-			targetExitDescriptions.add(exit);
+			this.targetExitDescriptions.add(exit);
 		}
 		else {
 			exit.addConduitDescription(conduit);
@@ -183,26 +188,26 @@ public class ConnectionScheme implements Serializable {
 
 		// see if conduit class is available
 		try {
-			Class qc = Class.forName( conduit.getClassName() );
+			Class.forName( conduit.getClassName() );
 		} catch (ClassNotFoundException e) {
 			throw new MUSCLERuntimeException(e);
-		}			
-		
+		}
+
 		conduit.addExitDescription(exit); // conduit has to connect to this exit
-		
+
 		return new Pipeline(entrance, conduit, exit);
 	}
 
 
 	//
 	public EntranceDescription entranceDescriptionForID(String entranceID) {
-		
-		for(Iterator<ExitDescription> iter = targetExitDescriptions.iterator(); iter.hasNext();) {
-			ExitDescription exit = iter.next();
+
+		for (ExitDescription exit : this.targetExitDescriptions) {
 			for(int i = 0; ; i++) {
 				ConduitDescription conduit = exit.getConduitDescription(i);
-				if( conduit == null)
+				if( conduit == null) {
 					break;
+				}
 				EntranceDescription entranceDescription = conduit.getEntranceDescription();
 				// see if the new entrance is in our connection scheme, otherwise we dont know how to connect this entrance
 				if(entranceDescription.getID().equals(entranceID)) {
@@ -217,9 +222,9 @@ public class ConnectionScheme implements Serializable {
 
 	//
 	public ExitDescription exitDescriptionForID(String exitID) {
-		
+
 		ExitDescription exitDescription = null;
-		for(Iterator<ExitDescription> iter = targetExitDescriptions.iterator(); iter.hasNext();) {
+		for(Iterator<ExitDescription> iter = this.targetExitDescriptions.iterator(); iter.hasNext();) {
 			exitDescription = iter.next();
 			// see if the new exit is in our connection scheme, otherwise we dont know how to connect this exit
 			if(exitDescription.getID().equals(exitID)) {
@@ -231,19 +236,19 @@ public class ConnectionScheme implements Serializable {
 		return exitDescription;
 	}
 
-	
+
 	//
 	public List<EntranceDescription> entranceDescriptionsForControllerID(AID controllerID) {
-		
+
 		LinkedList<EntranceDescription> entranceDescriptions = new LinkedList<EntranceDescription>();
-		for(Iterator<ExitDescription> iter = targetExitDescriptions.iterator(); iter.hasNext();) {
-			ExitDescription exit = iter.next();
+		for (ExitDescription exit : this.targetExitDescriptions) {
 			for(int i = 0; ; i++) {
 				ConduitDescription conduit = exit.getConduitDescription(i);
-				if( conduit == null)
+				if( conduit == null) {
 					break;
+				}
 				EntranceDescription description = conduit.getEntranceDescription();
-				
+
 				if(description.isAvailable() && description.getControllerID().equals(controllerID)) {
 					entranceDescriptions.add(description);
 				}
@@ -256,11 +261,9 @@ public class ConnectionScheme implements Serializable {
 
 	//
 	public List<ExitDescription> exitDescriptionsForControllerID(AID controllerID) {
-		
+
 		LinkedList<ExitDescription> exitDescriptions = new LinkedList<ExitDescription>();
-		for(Iterator<ExitDescription> iter = targetExitDescriptions.iterator(); iter.hasNext();) {
-			ExitDescription description = iter.next();
-			
+		for (ExitDescription description : this.targetExitDescriptions) {
 			if(description.isAvailable() && description.getControllerID().equals(controllerID)) {
 				exitDescriptions.add(description);
 			}
@@ -273,13 +276,13 @@ public class ConnectionScheme implements Serializable {
 	//
 	private ConduitDescription conduitDescriptionForNameIDEntrance(String className, String conduitID) {
 
-		for(Iterator<ExitDescription> iter = targetExitDescriptions.iterator(); iter.hasNext();) {
-			ExitDescription exit = iter.next();
+		for (ExitDescription exit : this.targetExitDescriptions) {
 			for(int i = 0; ; i++) {
 				ConduitDescription conduitDescription = exit.getConduitDescription(i);
-				if( conduitDescription == null)
+				if( conduitDescription == null) {
 					break;
-				
+				}
+
 				if(conduitDescription.getClassName().equals(className) && conduitDescription.getID().equals(conduitID)) {
 					return conduitDescription; // we located the new entrance in the global connection scheme
 				}
@@ -294,14 +297,14 @@ public class ConnectionScheme implements Serializable {
 	search thru our graph for exit descriptions whose kernels are currently not registered
 	*/
 	public List<ExitDescription> unconnectedExits() {
-		
+
 		LinkedList<ExitDescription> exitDescriptions = new LinkedList<ExitDescription>();
-		for(Iterator<ExitDescription> iter = targetExitDescriptions.iterator(); iter.hasNext();) {
-			ExitDescription exit = iter.next();
+		for (ExitDescription exit : this.targetExitDescriptions) {
 			for(int i = 0; ; i++) {
 				ConduitDescription conduit = exit.getConduitDescription(i);
-				if( conduit == null)
+				if( conduit == null) {
 					break;
+				}
 				if( !conduit.isAvailable() ) {
 					exitDescriptions.add(exit);
 				}
@@ -315,33 +318,36 @@ public class ConnectionScheme implements Serializable {
 
 public LinkedList<ExitDescription> getConnectionSchemeRoot() {
 
-	return targetExitDescriptions;
+	return this.targetExitDescriptions;
 }
 
 
 	//
+	@Override
 	public String toString() {
-	
+
 		StringWriter writer = new StringWriter();
 		PrintWriter out = new PrintWriter(writer);
-		
+
 		// print header
 		out.printf("\n========== target connection scheme <%s>\n  %-20s <- %-60s -< %-20s\n  ----------\n", ""/*COASTTool.getCXARessource("ConnectionScheme")*/, "<exit name>", "<conduit>#<id>(<ARG1,..,ARGN>)", "<entrance name>");
-		
-		for(ExitDescription exit : targetExitDescriptions) {
-		
+
+		for(ExitDescription exit : this.targetExitDescriptions) {
+
 			for(int i = 0; ; i++) {
 				ConduitDescription conduit = exit.getConduitDescription(i);
-				if( conduit == null)
+				if( conduit == null) {
 					break;
+				}
 				EntranceDescription entrance = conduit.getEntranceDescription();
 				String args = MiscTool.joinItems(java.util.Arrays.asList(conduit.getArgs()), ",");
-				if(args == null)
+				if(args == null) {
 					args = "";
+				}
 				out.printf("  %-20s <- %-60s -< %-20s\n", exit.getID(), conduit.getClassName()+"#"+conduit.getID()+"("+args+")", entrance.getID());
 			}
-		}		
-		
+		}
+
 		out.println("==========");
 
 		return writer.toString();
@@ -350,25 +356,27 @@ public LinkedList<ExitDescription> getConnectionSchemeRoot() {
 
 	//
 	public String availableConnectionsToString() {
-	
+
 		StringWriter writer = new StringWriter();
 		PrintWriter out = new PrintWriter(writer);
-		
+
 		// print header
 		out.printf("\n========== current active pipelines\n  %-20s <- %-60s <- %-20s\n  ----------\n", "<exit>", "<conduit>(<id>)", "<entrance>");
-		
-		for(ExitDescription exit : targetExitDescriptions) {		
+
+		for(ExitDescription exit : this.targetExitDescriptions) {
 			for(int i = 0; ; i++) {
 				ConduitDescription conduit = exit.getConduitDescription(i);
-				if( conduit == null)
+				if( conduit == null) {
 					break;
+				}
 				EntranceDescription entrance = conduit.getEntranceDescription();
-				
-				if( exit.isAvailable() && conduit.isAvailable() && entrance.isAvailable() )
-				out.printf("  %-20s <- %-60s <- %-20s\n", exit.getID(), conduit.getClassName()+"("+conduit.getID()+")", entrance.getID());
+
+				if( exit.isAvailable() && conduit.isAvailable() && entrance.isAvailable() ) {
+					out.printf("  %-20s <- %-60s <- %-20s\n", exit.getID(), conduit.getClassName()+"("+conduit.getID()+")", entrance.getID());
+				}
 			}
-		}		
-		
+		}
+
 		out.println("==========");
 
 		return writer.toString();
@@ -377,59 +385,132 @@ public LinkedList<ExitDescription> getConnectionSchemeRoot() {
 
 	// deserialize
 	private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
-		
+
 		// do default deserialization
 		in.defaultReadObject();
-		
+
 		// init transient fields
-		logger = muscle.logging.Logger.getLogger(ConnectionScheme.class);
+		this.logger = muscle.logging.Logger.getLogger(ConnectionScheme.class);
 	}
 
 
 	//
 	public void toView(ConnectionSchemeViewable view) {
 
-		for(ExitDescription exit : targetExitDescriptions) {
-		
+		for(ExitDescription exit : this.targetExitDescriptions) {
+
 			String[] parts = exit.getID().split("@");
-			if(parts.length != 2)
+			if(parts.length != 2) {
 				throw new RuntimeException("can not create vertex from <"+exit.getID()+">");
+			}
 			//Vertex v1 = new Vertex(exit.getControllerID().getName()); // might not be announced yet
 			Vertex v1 = new Vertex(parts[1], exit);
 			v1 = view.addVertex(v1);
 
 			for(int i = 0; ; i++) {
 				ConduitDescription conduit = exit.getConduitDescription(i);
-				if( conduit == null)
+				if( conduit == null) {
 					break;
+				}
 
 				EntranceDescription entrance = conduit.getEntranceDescription();
-	
+
 				parts = entrance.getID().split("@");
-				if(parts.length != 2)
+				if(parts.length != 2) {
 					throw new RuntimeException("can not create vertex from <"+entrance.getID()+">");
-				//Vertex v2 = new Vertex(entrance.getControllerID().getName()); // might not be announced yet 
-				Vertex v2 = new Vertex(parts[1], entrance); 
+				}
+				//Vertex v2 = new Vertex(entrance.getControllerID().getName()); // might not be announced yet
+				Vertex v2 = new Vertex(parts[1], entrance);
 				v2 = view.addVertex(v2);
 				view.addEdge(new Edge(conduit.getID(), conduit), v2, v1);
 			}
-		}	
+		}
 	}
-	
-	
+
+
 	//
 	public static class Pipeline {
 
 		public EntranceDescription entrance;
 		public ConduitDescription conduit;
 		public ExitDescription exit;
-		
+
 		public Pipeline(EntranceDescription newEntrance, ConduitDescription newConduit, ExitDescription newExit) {
-			
-			entrance = newEntrance;
-			conduit = newConduit;
-			exit = newExit;
+
+			this.entrance = newEntrance;
+			this.conduit = newConduit;
+			this.exit = newExit;
 		}
+	}
+	
+	public List kernelList; // for otf
+	public List conduitList; // for otf
+
+	// for otf
+	public String getEntrance(String dstSink) {
+		for (ExitDescription exit : targetExitDescriptions) {
+			for (int i = 0;; i++) {
+				ConduitDescription conduit = exit.getConduitDescription(i);
+				if (conduit == null)
+					break;
+				EntranceDescription entrance = conduit.getEntranceDescription();
+
+				conduitList.add(exit.getID());
+				conduitList.add(entrance.getID());
+
+				if (dstSink.equals(exit.getID()))
+					return (entrance.getID());
+			}
+		}
+		return "";
+	}
+
+	// for otf
+	public void generateLists() {
+		conduitList = new ArrayList();
+		kernelList = new ArrayList();
+
+		for (ExitDescription exit : targetExitDescriptions) {
+
+			for (int i = 0;; i++) {
+				ConduitDescription conduit = exit.getConduitDescription(i);
+				if (conduit == null)
+					break;
+				EntranceDescription entrance = conduit.getEntranceDescription();
+
+				conduitList.add(exit.getID());
+				
+				try{
+				//	System.out.println("Adding entrance" + entrance.getID());
+					conduitList.add(entrance.getID());					
+				}
+				catch (Exception e)
+				{
+					//conduitList.add(entrance.getID());
+				}
+				
+
+				String temp1 = exit.getID().substring(exit.getID().indexOf("@") + 1);
+				if (!kernelList.contains(temp1))
+					kernelList.add(temp1);
+
+				String temp2 = null;
+				try{
+					temp2 = entrance.getID().substring(entrance.getID().indexOf("@") + 1);
+					if (!kernelList.contains(temp2))
+					{
+						//System.out.println("Adding kernel "+temp2);
+						kernelList.add(temp2);
+					}
+				}
+				catch (Exception e)
+				{
+					
+				}
+			}
+		}
+		Collections.sort(kernelList);
+		Collections.sort(conduitList);
 	}
 
 
@@ -448,7 +529,7 @@ public LinkedList<ExitDescription> getConnectionSchemeRoot() {
 		catch(java.lang.IllegalAccessException e) {
 			throw new MUSCLERuntimeException(e);
 		}
-		
+
 		ConnectionSchemeJUNGPanel.create(cs);
 	}
 
