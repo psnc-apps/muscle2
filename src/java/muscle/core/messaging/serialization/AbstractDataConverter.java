4b4707f023cab031e5d6710b43d0c514026efaea
Joris Borgdorff <joris@jorisborgdorff.nl>
2012-02-02 09:54:34 +0000

    Added missing DataConverter
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/branches/filter@214 b725f6ce-2418-43e8-b868-1e0ad3d2b257

---
+++ src/java/muscle/core/messaging/serialization/AbstractDataConverter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muscle.core.messaging.serialization;
+
+/**
+ *
+ * @author Joris Borgdorff
+ */
+public abstract class AbstractDataConverter<E,F> implements DataConverter<E, F> {
+	public E copy(E data) {
+		return this.deserialize(this.serialize(data));
+	}
+}
