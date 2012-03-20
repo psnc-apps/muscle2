6d5e868b39673c253e671a025658563e53d3f97a
Bartosz Bosak <bbosak@man.poznan.pl>
2012-03-20 11:14:40 +0000

    Merge with cmake branch
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@315 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/cpp/muscle/includes.cmake
+++ src/cpp/muscle/includes.cmake
@@ -1,11 +1,10 @@
 #includes for the muscle core library
 
-GET_FILENAME_COMPONENT(PARENT_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
 
 # make sure the compiler can find include files for Java JNI
-INCLUDE(${PARENT_DIR}/FindJNI.cmake)
+INCLUDE(${CMAKE_SOURCE_DIR}/src/cpp/muscle/FindJNI.cmake)
 include_directories(${JAVA_INCLUDE_PATH}) # for jni.h
 include_directories(${JAVA_INCLUDE_PATH2}) # for jni_md.h
 
 # add our src root
-include_directories(${PARENT_DIR})
+include_directories(${CMAKE_SOURCE_DIR}/src/cpp/muscle)
