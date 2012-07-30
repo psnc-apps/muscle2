34994c0c56ddfc03f71df2c2effbc5efa4d702d3
Mariusz Mamonski <mamonski@man.poznan.pl>
2012-07-30 16:06:44 +0000

    update before build
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@459 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- build/build-galera.sh
+++ build/build-galera.sh
@@ -1,3 +1,6 @@
+cd ..
+svn up
+cd build
 export CMAKE_CXX_FLAGS='-g -Wall '
 export BOOST_INCLUDEDIR=/home/plgrid-groups/plggmuscle/deps/boost/include/
 export BOOST_LIBRARYDIR=/home/plgrid-groups/plggmuscle/deps/boost/lib/
@@ -5,6 +8,6 @@ export BOOST_LIBRARYDIR=/home/plgrid-groups/plggmuscle/deps/boost/lib/
 module load java
 module load openmpi
 
-cmake -DMUSCLE_INSTALL_PREFIX=/home/plgrid-groups/plggmuscle/2.0/ ..
+cmake -DMUSCLE_INSTALL_PREFIX=/home/plgrid-groups/plggmuscle/2.0/devel ..
 make
 make install
