492a039a87f4b50e762909898c5f746b38bfeb7d
Joris Borgdorff <joris@jorisborgdorff.nl>
2012-08-13 11:10:26 +0000

    Append last commit
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@498 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/cxa/LHC2.cxa.rb
+++ src/cxa/LHC2.cxa.rb
@@ -30,6 +30,7 @@ abort "this is a configuration file  to be used with the MUSCLE bootstrap utilit
 cxa = Cxa.LAST
 
 cxa.env["max_timesteps"] = 1
+cxa.env["default_dt"] = 1
 cxa.env["cxa_path"] = File.dirname(__FILE__)
 
 cxa.env["PSB:InitialEnergy"] = 1.2
