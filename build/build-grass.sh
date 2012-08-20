5deccd58e3e8fb82e997dcd2061eb0a900c3b624
Mariusz Mamonski <mamonski@man.poznan.pl>
2012-08-20 15:41:56 +0000

    Delete old build script
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@526 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- build/build-grass.sh
+++
@@ -1,7 +0,0 @@
-export CMAKE_CXX_FLAGS='-g -Wall '
-export BOOST_INCLUDEDIR=/usr/local/boost/include/
-export BOOST_LIBRARYDIR=/usr/local/boost/lib/
-
-cmake -DMUSCLE_INSTALL_PREFIX=/opt/app/muscle/2.0/devel ..
-make
-make install
