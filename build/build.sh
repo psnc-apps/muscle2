854117100fd1446a132b3d761721a42085830930
Mariusz Mamonski <mamonski@man.poznan.pl>
2012-08-20 15:37:16 +0000

    WCSS configuration
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@524 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- build/build.sh
+++ build/build.sh
@@ -6,6 +6,11 @@ cd build
 #2. Source local configuration
 . ${HOSTNAME:-`hostname`}.conf
 
+if [ -f "$PBS_O_HOST.conf" ]
+then
+	. $PBS_O_HOST.conf
+fi
+
 #3. Build release
 cmake -DMUSCLE_INSTALL_PREFIX=$INSTALL_PREFIX/devel -DCMAKE_BUILD_TYPE=Release ..
 make
