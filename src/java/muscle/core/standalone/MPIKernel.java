e9c70ead27bc708d4074bc17f2be2f4c95460000
Joris Borgdorff <joris@jorisborgdorff.nl>
2012-08-23 15:52:16 +0000

    MPIKernel complains if it can't find its parameters
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@544 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/java/muscle/core/standalone/MPIKernel.java
+++ src/java/muscle/core/standalone/MPIKernel.java
@@ -2,9 +2,12 @@ package muscle.core.standalone;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import muscle.core.CxADescription;
 
 public class MPIKernel extends NativeKernel {
+	private final static Logger logger = Logger.getLogger(MPIKernel.class.getName());
 
 	/**
 	 *  Default serial versionUID
@@ -13,14 +16,19 @@ public class MPIKernel extends NativeKernel {
 
 	@Override
 	protected void buildCommand(List<String> command) {
-		if (CxADescription.ONLY.containsKey(getLocalName() + ":mpiexec_command"))
-			command.add(CxADescription.ONLY.getProperty(getLocalName() + ":mpiexec_command"));
-		else
+		if (hasProperty("mpiexec_command")) {			
+			command.add(getProperty("mpiexec_command"));
+		}
+		else {
+			logger.warning("MPI command variable ''mpiexec_command'' not given. Using mpiexec.");
 			command.add("mpiexec");
+		}
 		
-		if (CxADescription.ONLY.containsKey(getLocalName() + ":mpiexec_args")) {
-			String args[] = CxADescription.ONLY.getProperty(getLocalName() + ":mpiexec_args").split(" ");
+		if (hasProperty("mpiexec_args")) {
+			String args[] = getProperty("mpiexec_args").split(" ");
 			command.addAll(Arrays.asList(args));
+		} else {
+			logger.log(Level.WARNING, "MPI arguments variable ''mpiexec_args'' for {0} not given. Using mpiexec.", getLocalName());
 		}
 		
 		super.buildCommand(command);
