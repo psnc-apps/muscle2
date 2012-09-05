21bc8f8dda212872c940ae35fd74adb767e81a22
Joris Borgdorff <joris@jorisborgdorff.nl>
2012-09-05 16:37:43 +0000

    Reduced time interval waiting for manager in muscle.cls.rb, and replaced erroneous ++ with +=
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@556 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/ruby/muscle.cls.rb
+++ src/ruby/muscle.cls.rb
@@ -13,7 +13,7 @@ class Muscle
 	def initialize
 		@@LAST = self
 		@env = {}
-    
+		
 		# set value for LIBPATHENV or abort
 		assert_LIBPATHENV @env
 
@@ -73,149 +73,149 @@ class Muscle
 	end
 	
 	def print_env(keys)
-	  if keys == true
-  		# print the complete env (sorted)
-  		env.keys.sort.each {|k| puts "#{k.inspect}=>#{env[k].inspect}"}
-  	else
-  		# print value for the specified key(s)
-  		if(keys.size == 1)
-  			# print raw value if output is for a single key (useful if you want to further process the output, e.g. CLASSPATH)
-  			puts env[keys.first] if env.has_key? keys.first
-  		else
-  			keys.each {|k| puts "#{k.inspect}=>#{env[k].inspect}" if env.has_key? k}
-  		end
-  	end
+		if keys == true
+			# print the complete env (sorted)
+			env.keys.sort.each {|k| puts "#{k.inspect}=>#{env[k].inspect}"}
+		else
+			# print value for the specified key(s)
+			if(keys.size == 1)
+				# print raw value if output is for a single key (useful if you want to further process the output, e.g. CLASSPATH)
+				puts env[keys.first] if env.has_key? keys.first
+			else
+				keys.each {|k| puts "#{k.inspect}=>#{env[k].inspect}" if env.has_key? k}
+			end
+		end
 	end
 	
 	def run_manager(args)
-	  tmpXmx = env['Xmx']
-  	tmpXms = env['Xms']
-  	env['Xms'] = '20m'
-  	env['Xmx'] = '100m'
-  	command = JVM.build_command(args, env).first
-  	env['Xms'] = tmpXms
-  	env['Xmx'] = tmpXmx
-  	puts "=== Running MUSCLE2 Simulation Manager ==="
-  	if env['verbose']
-  		puts "Executing: " + command
-  	end
-  	return Process.fork {exec(command)}
-  end
+		tmpXmx = env['Xmx']
+		tmpXms = env['Xms']
+		env['Xms'] = '20m'
+		env['Xmx'] = '100m'
+		command = JVM.build_command(args, env).first
+		env['Xms'] = tmpXms
+		env['Xmx'] = tmpXmx
+		puts "=== Running MUSCLE2 Simulation Manager ==="
+		if env['verbose']
+			puts "Executing: " + command
+		end
+		return Process.fork {exec(command)}
+	end
 	
 	def run_client(args, contact_file_name = nil)
-	  args << "-m"
-	  if env['manager']
-      	args << env['manager']
-    else
-      # main
-    	tries_count = 0
+		args << "-m"
+		if env['manager']
+				args << env['manager']
+		else
+			# main
+			tries_count = 0
 
-  		while !File.exists?(contact_file_name)
-  			sleep 2
-  			if ++tries_count % 10 == 0
-  				puts "Waiting for manager contact file: #{contact_file_name}"
-  			end
-  		end
+			while !File.exists?(contact_file_name)
+				sleep 0.2
+                tries_count += 1
+				if tries_count % 25 == 0
+					puts "Waiting for manager contact file: #{tries_count} #{contact_file_name}"
+				end
+			end
 
-  		while File.exists?(contact_file_name + ".lock") #waiting for lock file to disappear 
-  			sleep 2
-  		end
+			while File.exists?(contact_file_name + ".lock") #waiting for lock file to disappear 
+				sleep 0.2
+			end
 
-  		contact_addr = File.open(contact_file_name, "rb").read
-  		puts "Using manager address: #{contact_addr}"
-  		args << contact_addr
-  	end
+			contact_addr = File.open(contact_file_name, "rb").read
+			puts "Using manager address: #{contact_addr}"
+			args << contact_addr
+		end
 
-  	puts "=== Running MUSCLE2 Simulation ==="
-    command = JVM.build_command(args, env).first
-  	if env['verbose']
-  		puts command
-  	end
-  	return Process.fork {exec(command)}
-  end
+		puts "=== Running MUSCLE2 Simulation ==="
+		command = JVM.build_command(args, env).first
+		if env['verbose']
+			puts command
+		end
+		return Process.fork {exec(command)}
+	end
 	
 	def exec_native(kernel_name, extra_args)
-  	
-    native_command = []
-    cxa = Cxa.LAST
-  	if cxa.env[ kernel_name + ":mpiexec_command"] 
-  		native_command << cxa.env[ kernel_name + ":mpiexec_command"]
-  	end
+		native_command = []
+		cxa = Cxa.LAST
+		if cxa.env[ kernel_name + ":mpiexec_command"] 
+			native_command << cxa.env[ kernel_name + ":mpiexec_command"]
+		end
 
-  	if cxa.env[ kernel_name + ":mpiexec_args"] 
-  		native_command << cxa.env[ kernel_name + ":mpiexec_args"].split(" ")
-  	end
+		if cxa.env[ kernel_name + ":mpiexec_args"] 
+			native_command << cxa.env[ kernel_name + ":mpiexec_args"].split(" ")
+		end
 
-  	if cxa.env[kernel_name + ":debugger"] 
-  		native_command << cxa.env[ kernel_name + ":debugger"]
-  	end
+		if cxa.env[kernel_name + ":debugger"] 
+			native_command << cxa.env[ kernel_name + ":debugger"]
+		end
 
-  	if cxa.env[ kernel_name + ":command"] 
-  		native_command << cxa.env[ kernel_name + ":command"]
-  	else
-  		puts "Missing " + kernel_name + ":command property"
-  		exit 1
-  	end
+		if cxa.env[ kernel_name + ":command"] 
+			native_command << cxa.env[ kernel_name + ":command"]
+		else
+			puts "Missing " + kernel_name + ":command property"
+			exit 1
+		end
 
-  	if cxa.env[ kernel_name + ":args"] 
-  		puts "Args: " + cxa.env[ kernel_name + ":args"] 
-  		native_command << cxa.env[ kernel_name + ":args"].split(" ")
-  	end
+		if cxa.env[ kernel_name + ":args"] 
+			puts "Args: " + cxa.env[ kernel_name + ":args"] 
+			native_command << cxa.env[ kernel_name + ":args"].split(" ")
+		end
 
-  	extra_args.delete("--native");
+		extra_args.delete("--native");
 
-  	native_command << "--"
-  	native_command << extra_args
-  	
-  	command = native_command.join(" ")
-  	
-  	puts "Executing: " + command
-  	Process.exec(command)
+		native_command << "--"
+		native_command << extra_args
+		
+		command = native_command.join(" ")
+		
+		puts "Executing: " + command
+		Process.exec(command)
 	end
 	
 	def exec_mpi(args)
-	  command = JVM.build_command(args, env).first
-	  puts "Executing: " + command
-	  Process.exec(command)
+		command = JVM.build_command(args, env).first
+		puts "Executing: " + command
+		Process.exec(command)
 	end
 	
 	def apply_intercluster
-    port_min = env['port_min'] || ENV['MUSCLE_PORT_MIN']
-    port_max = env['port_max'] || ENV['MUSCLE_PORT_MAX']
-    if(port_min.nil? or port_max.nil?)
-  	  puts "Warning: intercluster specified, but no local port range given! Intercluster ignored."
-    else
-    	mto =  env['mto'] || ENV['MUSCLE_MTO']
-    	if (! mto.nil?)
-    		mtoHost = mto.split(':')[0]
-    		mtoPort = mto.split(':')[1]
-    	end
+		port_min = env['port_min'] || ENV['MUSCLE_PORT_MIN']
+		port_max = env['port_max'] || ENV['MUSCLE_PORT_MAX']
+		if(port_min.nil? or port_max.nil?)
+			puts "Warning: intercluster specified, but no local port range given! Intercluster ignored."
+		else
+			mto =	env['mto'] || ENV['MUSCLE_MTO']
+			if (! mto.nil?)
+				mtoHost = mto.split(':')[0]
+				mtoPort = mto.split(':')[1]
+			end
 
-    	if(mtoPort.nil? or mtoHost.nil?)
-    	  puts "Warning: intercluster specified, but no MTO address/port given! Intercluster ignored."
-    	else
-    	  if(env.has_key?('qcg'))
-    	  	if (env['main'])
-    			  env['bindport'] = 22 #master
-    		  else
-    			  env['manager'] = "localhost:22" #slave
-    		  end
-    	  else
-    		  env['localport'] = 0 
-    	  end
+			if(mtoPort.nil? or mtoHost.nil?)
+				puts "Warning: intercluster specified, but no MTO address/port given! Intercluster ignored."
+			else
+				if(env.has_key?('qcg'))
+					if (env['main'])
+						env['bindport'] = 22 #master
+					else
+						env['manager'] = "localhost:22" #slave
+					end
+				else
+					env['localport'] = 0 
+				end
 
-    	  if(env["jvmflags"].nil?)
-    		  env["jvmflags"] = Array.new
-    	  end
+				if(env["jvmflags"].nil?)
+					env["jvmflags"] = Array.new
+				end
 
-    	  env["jvmflags"] << "-Dpl.psnc.muscle.socket.factory=muscle.net.CrossSocketFactory"
-    	  env["jvmflags"] << "-D" + PROP_PORT_RANGE_MIN + "=" + port_min
-    	  env["jvmflags"] << "-D" + PROP_PORT_RANGE_MAX + "=" + port_max
-    	  env["jvmflags"] << "-D" + PROP_MTO_ADDRESS    + "=" + mtoHost
-    	  env["jvmflags"] << "-D" + PROP_MTO_PORT       + "=" + mtoPort.to_s
+				env["jvmflags"] << "-Dpl.psnc.muscle.socket.factory=muscle.net.CrossSocketFactory"
+				env["jvmflags"] << "-D" + PROP_PORT_RANGE_MIN + "=" + port_min
+				env["jvmflags"] << "-D" + PROP_PORT_RANGE_MAX + "=" + port_max
+				env["jvmflags"] << "-D" + PROP_MTO_ADDRESS		+ "=" + mtoHost
+				env["jvmflags"] << "-D" + PROP_MTO_PORT			 + "=" + mtoPort.to_s
 
-    	end
-    end
+			end
+		end
 	end
 	
 	# visibility
