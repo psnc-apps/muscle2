1ae2de1550b645d4601d43c372f2a23139153d1b
Joris Borgdorff <joris@jorisborgdorff.nl>
2012-11-01 16:54:05 +0000

    [Java] Formatting logger error message with full stack trace
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@684 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/java/muscle/util/logging/ModuleFormatter.java
+++ src/java/muscle/util/logging/ModuleFormatter.java
@@ -5,6 +5,8 @@
 
 package muscle.util.logging;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
@@ -14,7 +16,7 @@ import java.util.logging.SimpleFormatter;
  * @author Joris Borgdorff
  */
 public class ModuleFormatter extends SimpleFormatter {
-	private final static String format = "(%tT %6.6s) %s%s\n%s";
+	private final static String format = "(%tT %6.6s) %s%s%s\n";
 	private final static int SEVERE = Level.SEVERE.intValue();
 	private final static int WARNING = Level.WARNING.intValue();
 	private final static int INFO = Level.INFO.intValue();
@@ -45,7 +47,17 @@ public class ModuleFormatter extends SimpleFormatter {
 		if(thrown == null) {
 			err = "";
 		} else {
-			err = "                         (" + thrown.getClass().getName() + ": " + thrown.getMessage() + ")\n";
+			try {
+				StringWriter sw = new StringWriter();
+				PrintWriter pw = new PrintWriter(sw);
+				pw.println("\n[================== ERROR ===================] " + thrown.getClass().getName() + ": " + thrown.getMessage());
+				thrown.printStackTrace(pw);
+				pw.print("[================ END TRACE =================]");
+				pw.close();
+				err = sw.toString();
+			} catch (Exception ex) {
+				err = "";
+			}
 		}
 		
 		return String.format(format, System.currentTimeMillis(), this.name, level, msg, err);
