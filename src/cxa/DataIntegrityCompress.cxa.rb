6c31ad60b316c2a457f9cf3694afaf844f0d8fdf
Joris Borgdorff <joris@jorisborgdorff.nl>
2012-12-06 14:01:38 +0000

    [CxA] Different compression strategies for DataIntegrity
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@741 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/cxa/DataIntegrityCompress.cxa.rb
+++ src/cxa/DataIntegrityCompress.cxa.rb
@@ -22,9 +22,27 @@ cs = cxa.cs
 
 cs.attach('Check' => 'Bounce') {
 	tie('datatype', 'datatype')
-	tie('out', 'in',["serialize","chunk_32","thread","compress"],["decompress", "dechunk_32", "deserialize"])
+	# 4.71s user 0.66s system 81% cpu 6.566 total
+	#tie('out', 'in',["serialize","chunk_32","thread","compress"],["decompress", "dechunk_32", "deserialize"])
+	# 4.71s user 0.63s system 80% cpu 6.602 total 
+	#tie('out', 'in',["serialize","chunk_32","compress"],["decompress", "dechunk_32", "deserialize"])
+	# 4.00s user 0.55s system 79% cpu 5.708 total
+	#tie('out', 'in',["serialize","chunk_4","thread","compress"],["decompress", "dechunk_4", "deserialize"])
+	# 3.98s user 0.55s system 80% cpu 5.641 total
+	#tie('out', 'in',["serialize","compress"],["decompress", "deserialize"])
+	# 3.93s user 0.55s system 79% cpu 5.617 total
+	tie('out', 'in',["serialize","chunk_4","compress","thread"],["decompress", "dechunk_4", "deserialize"])
 }
 
 cs.attach('Bounce' => 'Check') {
-	tie('out', 'in',["serialize", "chunk_32", "thread","compress"],["decompress", "dechunk_32", "deserialize"])
+	# 4.71s user 0.66s system 81% cpu 6.566 total
+	#tie('out', 'in',["serialize", "chunk_32", "thread","compress"],["decompress", "dechunk_32", "deserialize"])
+	# 4.71s user 0.63s system 80% cpu 6.602 total
+	#tie('out', 'in',["serialize", "chunk_32", "compress"],["decompress", "dechunk_32", "deserialize"])
+	# 4.00s user 0.55s system 79% cpu 5.708 total
+	#tie('out', 'in',["serialize","chunk_4","thread","compress"],["decompress", "dechunk_4", "deserialize"])
+	# 3.98s user 0.55s system 80% cpu 5.641 total
+	#tie('out', 'in',["serialize", "compress"],["decompress", "deserialize"])
+	# 3.93s user 0.55s system 79% cpu 5.617 total
+	tie('out', 'in',["serialize","chunk_4","compress","thread"],["decompress", "dechunk_4", "deserialize"])
 }
