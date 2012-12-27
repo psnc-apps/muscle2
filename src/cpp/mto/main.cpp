c351a567327cd4224aa554aee9db4256d2cf362d
Mariusz Mamonski <mamonski@man.poznan.pl>
2012-12-27 17:05:28 +0000

    [MTO] - TCP Buffer size on initator side
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@769 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/cpp/mto/main.cpp
+++ src/cpp/mto/main.cpp
@@ -39,6 +39,8 @@ void startConnectingToPeers();
 /** Starts establishing connection with a peer proxy */
 void startConnectingToPeer(boost::asio::ip::tcp::endpoint where);
 
+void setSocketOptions(tcp::socket *_sock);
+
 void writeHellos(tcp::socket * sock);
 
 /** Parses the Hello and returns if this hello should be forwarded to others */
@@ -280,6 +282,8 @@ void StubbornConnecter::connectSucceeded()
   Logger::trace(Logger::MsgType_PeerConn, "Connected to peer  %s:%hu, starting hello exchange",
       where.address().to_string().c_str(), where.port()
     );
+
+  setSocketOptions(sock);
   writeHellos(sock);
   HelloReader(sock,
               function<void(const error_code & ec)>(bind(&StubbornConnecter::allHellosRead, this, placeholders::error())),
@@ -317,31 +321,7 @@ public:
 InitPeerConnection::InitPeerConnection(tcp::socket* _sock)
    : sock(_sock)
 {
-  boost::asio::socket_base::keep_alive keep_alive_option(true);
-  boost::asio::ip::tcp::no_delay no_delay_option(true);
-  _sock->set_option(keep_alive_option);
-  _sock->set_option(no_delay_option);
-  
-  if (Options::getInstance().getTCPBufSize() != 0) 
-  {
-	boost::asio::socket_base::receive_buffer_size receive_buffer_option( Options::getInstance().getTCPBufSize() );
-	boost::asio::socket_base::send_buffer_size send_buffer_option(  Options::getInstance().getTCPBufSize() );
-
-  	Logger::info(Logger::MsgType_Config|Logger::MsgType_PeerConn,"Setting custom TCP buffers sizes");
-	_sock->set_option(receive_buffer_option);
-	_sock->set_option(send_buffer_option);
-  } 
-
-  boost::asio::socket_base::receive_buffer_size receive_buffer_option;
-  boost::asio::socket_base::send_buffer_size send_buffer_option;
-
-  _sock->get_option(send_buffer_option);
-  _sock->get_option(receive_buffer_option);
-
-  Logger::info(Logger::MsgType_Config|Logger::MsgType_PeerConn,"TCP bufers  send=%dkb recv=%dkb",
-	(int)(send_buffer_option.value()/1024),
-	(int)(receive_buffer_option.value()/1024));
-
+  setSocketOptions(_sock);
 
   buf = new char[MtoHello::getSize()];
   function< void(error_code ec) > callback( bind(&InitPeerConnection::allHellosReceived, this, placeholders::error()));
@@ -789,5 +769,35 @@ int main(int argc, char **argv)
   while(!e) ioService.run(e);
   
   return 0;
+}
+
+
+void setSocketOptions(tcp::socket *_sock)
+{
+  Logger::info(Logger::MsgType_Config|Logger::MsgType_PeerConn,"Setting custom socket options");
+  boost::asio::socket_base::keep_alive keep_alive_option(true);
+  boost::asio::ip::tcp::no_delay no_delay_option(true);
+  _sock->set_option(keep_alive_option);
+  _sock->set_option(no_delay_option);
+  
+  if (Options::getInstance().getTCPBufSize() != 0) 
+  {
+	boost::asio::socket_base::receive_buffer_size receive_buffer_option( Options::getInstance().getTCPBufSize() );
+	boost::asio::socket_base::send_buffer_size send_buffer_option(  Options::getInstance().getTCPBufSize() );
+
+  	Logger::info(Logger::MsgType_Config|Logger::MsgType_PeerConn,"Setting custom TCP buffers sizes");
+	_sock->set_option(receive_buffer_option);
+	_sock->set_option(send_buffer_option);
+  } 
+
+  boost::asio::socket_base::receive_buffer_size receive_buffer_option;
+  boost::asio::socket_base::send_buffer_size send_buffer_option;
+
+  _sock->get_option(send_buffer_option);
+  _sock->get_option(receive_buffer_option);
+
+  Logger::info(Logger::MsgType_Config|Logger::MsgType_PeerConn,"TCP bufers  send=%dkb recv=%dkb",
+	(int)(send_buffer_option.value()/1024),
+	(int)(receive_buffer_option.value()/1024));
 }
 
