83e2f5e8672cad5271d17bc4fc61925b85ccb5cd
Joris Borgdorff <joris@jorisborgdorff.nl>
2013-05-13 15:35:55 +0000

    [Java/Examples] Added missing graphics panel
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@823 b725f6ce-2418-43e8-b868-1e0ad3d2b257

---
+++ src/java/examples/laplace/graphics/GraphicsPanel.java
@@ -0,0 +1,98 @@
+/*
+ * 
+ */
+
+package examples.laplace.graphics;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Joris Borgdorff
+ */
+public class GraphicsPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private final int nx, ny, dx;
+	private double[][] data = null;
+	
+	public GraphicsPanel(int nx, int ny, int dx) {
+		this.dx = dx;
+		this.nx = nx;
+		this.ny = ny;
+		setPreferredSize(new Dimension(nx * dx, ny * dx));
+	}
+	
+	public void show(String name, int xdiff, int ydiff) {
+		JFrame frame = new JFrame(name);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(this);
+
+		frame.setLocation((int) frame.getLocation().getX() + xdiff, (int) frame.getLocation().getY() + ydiff);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public synchronized void paintAndWait(double[][] newData) {
+		synchronized (this) {
+			while (data != null) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			data = newData;
+		}
+		repaint();
+	}
+	
+	protected void paintComponent(Graphics gg) {
+		synchronized (this) {
+			if (data == null) return;
+		}
+		super.paintComponent(gg);
+		Graphics2D g = (Graphics2D) gg;
+		// fill with initial conditions
+		for (int x = 0; x < nx; x++) {
+			for (int y = 0; y < ny; y++) {
+				g.setColor(getColor(data[x][y]));
+				g.fillRect(x * dx, (ny - 1) * dx - 1 * y * dx, dx, dx);
+			}
+		}
+		synchronized (this) {
+			data = null;
+			notify();
+		}
+	}
+	
+	/**
+	 * create a colour gradient from blue to red from the input values
+	 * -1.0..1.0<br>
+	 * -1.0 -0.5 0.0 0.5 1.0 | | | | | r 000::::000::::000<<<<255::::255 g
+	 * 000<<<<255::::255::::255>>>>000 b 255::::255>>>>000::::000::::000
+	 *
+	 * @author Jan Hegewald
+	 */
+	private static Color getColor(double rawVal) {
+		// val should be from -1 to 1
+		// first we apply val to our colour range, which is from 0...4*256
+		int val = (int) ((rawVal + 1) / 2.0 * 1023.0);
+		assert val >= 0 && val < 4*256;
+		
+		if (val < 256) {
+			return new Color(0, val, 255);
+		} else if (val < 2 * 256) {
+			return new Color(0, 255, 2*256 - 1 - val);
+		} else if (val < 3 * 256) {
+			return new Color(val - 2 * 256, 255, 0);
+		} else {
+			return new Color(255, 4 * 256 - 1 - val, 0);
+		}
+	}
+}
