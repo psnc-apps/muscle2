45f064a4f361a85387f89f0b4c4a71ef11fb6aef
Joris Borgdorff <joris@jorisborgdorff.nl>
2013-07-23 11:22:58 +0000

    Include/exclude files of last commit
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@865 b725f6ce-2418-43e8-b868-1e0ad3d2b257

---
+++ src/cpp/mto/util/rwmutex.cpp
@@ -0,0 +1,106 @@
+//
+//  rwmutex.cpp
+//  CMuscle
+//
+//  Created by Joris Borgdorff on 15-07-13.
+//  Copyright (c) 2013 Joris Borgdorff. All rights reserved.
+//
+
+#include "rwmutex.h"
+#include "../../muscle2/exception.hpp"
+
+using namespace muscle;
+
+rwmutex_lock::rwmutex_lock(bool read, pthread_rwlock_t *m, bool onlyTry) : mut(m)
+{
+    if (onlyTry)
+    {
+        int res;
+        if (read)
+            res = pthread_rwlock_tryrdlock(mut);
+        else
+            res = pthread_rwlock_trywrlock(mut);
+        if (res != 0)
+            mut = NULL;
+    }
+    else
+    {
+        if (read)
+            pthread_rwlock_rdlock(mut);
+        else
+            pthread_rwlock_wrlock(mut);
+    }
+    
+    refs = new int(1);
+}
+
+rwmutex_lock::rwmutex_lock(const rwmutex_lock &other) : mut(other.mut), refs(other.refs)
+{
+    ++(*refs);
+}
+
+rwmutex_lock::~rwmutex_lock()
+{
+    if (--(*refs) == 0)
+    {
+		if (mut)
+			pthread_rwlock_unlock(mut);
+		delete refs;
+    }
+}
+
+bool rwmutex_lock::isValid() const
+{
+    return mut != NULL;
+}
+
+rwmutex::rwmutex()
+{
+    int ret;
+    mut = new pthread_rwlock_t;
+    if ((ret = pthread_rwlock_init(mut, NULL)) != 0)
+    {
+        delete mut;
+        throw muscle::muscle_exception("Could not initialize mutex", ret);
+    }
+    
+    refs = new int(1);
+}
+
+rwmutex::rwmutex(const rwmutex& other) : mut(other.mut), refs(other.refs)
+{
+    ++(*refs);
+}
+
+rwmutex::~rwmutex()
+{
+    if (--(*refs) == 0)
+    {
+        // Make sure that there is no active lock anymore
+        pthread_rwlock_destroy(mut);
+        delete mut;
+        delete refs;
+    }
+}
+
+rwmutex_lock rwmutex::acquireRead() const
+{
+    return rwmutex_lock(true, mut, false);
+}
+
+rwmutex_lock rwmutex::acquireWrite() const
+{
+    return rwmutex_lock(false, mut, false);
+}
+
+rwmutex_lock *rwmutex::tryWrite() const
+{
+    rwmutex_lock *lock = new rwmutex_lock(false, mut, true);
+
+    if (lock->isValid())
+        return lock;
+    else {
+        delete lock;
+	    return NULL;
+    }
+}
