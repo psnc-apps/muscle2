e4e701bcac42c0971f6f73cfd91e3047bb0fa176
Joris Borgdorff <joris@jorisborgdorff.nl>
2013-08-02 15:42:45 +0000

    Updated MPWide to the current version
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@894 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/cpp/mto/mpwide/mpwide-macros.h
+++ src/cpp/mto/mpwide/mpwide-macros.h
@@ -9,24 +9,26 @@
 #ifndef CMuscle_mpwide_macros_h
 #define CMuscle_mpwide_macros_h
 
+#define MAX_NUM_STREAMS 10000
+#define MAX_NUM_PATHS 1000
 
 /* Enable (define)/Disable(don't define) Performance Timing Measurements */
-#define PERF_TIMING
-/* Performance report verbosity: 1 reports speeds on send/recv. 2 reports on initialization details.
- 3 also reports number of steps taken to recv packages. 4 becomes ridiculously verbose, with e.g.
- reports for accumulated bytes after every chunk is received. */
-#define PERF_REPORT 2
+//#define PERF_TIMING
+
 // Run a performance monitoring thread
-#define MONITORING 1
+#define MONITORING 0
 
 // Report the buffer sizes of sockets
-#define REPORT_BUFFERSIZES 1
+#define REPORT_BUFFERSIZES 0
 
 // Immediately exit if a send or receive error is found
-#define EXIT_ON_SENDRECV_ERROR 1
+#define EXIT_ON_SENDRECV_ERROR 0
 
 // Use a smaller number of streams for small messages 
 #define OptimizeStreamCount 1
+// standard maximum segment size * 2.
+#define BytesPerStream (2*1380)
+
 /* TimeOut in milliseconds. 0 means no timeout */
 #define InitStreamTimeOut 1
 
@@ -39,29 +41,42 @@
 #define LVL_WARN 2
 #define LVL_INFO 4
 #define LVL_DEBUG 6
+#define LVL_TRACE 6
 
 // SET THE LOG LEVEL
-#define LOG_LVL LVL_ERR
+#define LOG_LVL LVL_NONE
+
+#if LOG_LVL > LVL_NONE
+#include <pthread.h>
+static pthread_mutex_t log_mutex__ = PTHREAD_MUTEX_INITIALIZER;
+#endif
 
 #if LOG_LVL >= LVL_ERR
-#define LOG_ERR(MSG) cout << MSG << endl
+
+#define DO_LOG_(MSG) { pthread_mutex_lock(&log_mutex__); cout << MSG << endl; pthread_mutex_unlock(&log_mutex__); }
+#define LOG_ERR(MSG) DO_LOG_(MSG)
 #else
 #define LOG_ERR(MSG)
 #endif
 #if LOG_LVL >= LVL_WARN
-#define LOG_WARN(MSG) cout << MSG << endl
+#define LOG_WARN(MSG) DO_LOG_(MSG)
 #else
 #define LOG_WARN(MSG)
 #endif
 #if LOG_LVL >= LVL_INFO
-#define LOG_INFO(MSG) cout << MSG << endl
+#define LOG_INFO(MSG) DO_LOG_(MSG)
 #else
 #define LOG_INFO(MSG)
 #endif
 #if LOG_LVL >= LVL_DEBUG
-#define LOG_DEBUG(MSG) cout << MSG << endl
+#define LOG_DEBUG(MSG) DO_LOG_(MSG)
 #else
 #define LOG_DEBUG(MSG)
+#endif
+#if LOG_LVL >= LVL_TRACE
+#define LOG_TRACE(MSG) DO_LOG_(MSG)
+#else
+#define LOG_TRACE(MSG)
 #endif
 
 #define max(X,Y) ((X) > (Y) ? (X) : (Y))
