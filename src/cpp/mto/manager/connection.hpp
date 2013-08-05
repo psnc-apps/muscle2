9e40f77ec7dd2c0c05b2b0951eef3208f814369f
Joris Borgdorff <joris@jorisborgdorff.nl>
2013-08-05 13:59:02 +0000

    [MTO] use even larger connection buffer
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@908 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/cpp/mto/manager/connection.hpp
+++ src/cpp/mto/manager/connection.hpp
@@ -11,8 +11,8 @@
 #include <exception>
 
 /** Size of a receive data buffer for each connection
- With MTU=1380, this is 512 packets */
-#define MTO_CONNECTION_BUFFER_SIZE 2826240
+ With MTU=1460, this is 4096 packets */
+#define MTO_CONNECTION_BUFFER_SIZE 5980160
 
 class PeerCollection;
 class PeerConnectionHandler;
