ba6e709ec121da452f968764750cdf84b3e3a276
Mariusz Mamonski <mamonski@man.poznan.pl>
2013-11-11 17:12:18 +0000

    Matlab kernel
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@973 b725f6ce-2418-43e8-b868-1e0ad3d2b257

---
+++ src/java/muscle/core/standalone/MatlabKernel.java
@@ -0,0 +1,81 @@
+/*
+* Copyright 2008, 2009 Complex Automata Simulation Technique (COAST) consortium
+* Copyright 2010-2013 Multiscale Applications on European e-Infrastructures (MAPPER) project
+*
+* GNU Lesser General Public License
+* 
+* This file is part of MUSCLE (Multiscale Coupling Library and Environment).
+* 
+* MUSCLE is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* MUSCLE is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with MUSCLE.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package muscle.core.standalone;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+  * A kernel that executes a native instance with MPI.
+  * 
+  * For this to work correctly, the mpiexec_command parameter should be set.
+  */
+public class MatlabKernel extends NativeKernel {
+	private final static Logger logger = Logger.getLogger(MPIKernel.class.getName());
+
+	/**
+	 *  Default serial versionUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void buildCommand(List<String> command) {
+		if (hasProperty("matlab_command")) {			
+			command.add(getProperty("matlab_command"));
+		}
+		else {
+			logger.log(Level.WARNING, "MATLAB command variable ''matlab_command''  for {0} not given. Using matlab.", getLocalName());
+			command.add("matlab");
+		}
+		
+		if (hasProperty("matlab_args")) {
+			String args[] = getProperty("matlab_args").trim().split(" ");
+			command.addAll(Arrays.asList(args));
+		} else {
+			logger.log(Level.WARNING, "MATLAB arguments variable ''matlab_args'' for {0} not given. Not using arguments.", getLocalName());
+		}
+//-nosplash  -nodisplay
+		command.add("-nosplash");
+		command.add("-nodisplay");
+//-r \"addpath\(\'$PWD\'\)\; try, $MAIN_FUNCTION, catch Ex, Error = Ex, ErrorReport = getReport\(Ex, \'extended\'\), ErrorStack = Ex.stack, exit\(4\), end\; exit\"		
+		if (hasInstanceProperty("script")) {
+			File script = new File(getProperty("script"));
+			String scriptDir = script.getParent();
+			String mainFunction = script.getName().split("\\.")[0];
+			command.add("-r");
+			command.add("addpath('" + scriptDir+ "'); try, " + mainFunction + ", catch Ex, Error = Ex," +
+				" ErrorReport = getReport(Ex, 'extended'), ErrorStack = Ex.stack, exit(4), end; exit");
+		}
+		else {
+			throw new IllegalArgumentException("Missing property: " + getLocalName() + ":script" );
+		}
+		
+		if (hasInstanceProperty("args")) {
+			String args[] = getProperty("args").split(" ");
+			command.addAll(Arrays.asList(args));
+		}
+
+	}
+
+}
