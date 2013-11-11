3f09758e9a57fa2897fdd9453ea7c131301d3d7d
Mariusz Mamonski <mamonski@man.poznan.pl>
2013-11-11 17:14:22 +0000

    Example Matlab CxA
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@974 b725f6ce-2418-43e8-b868-1e0ad3d2b257

---
+++ src/cxa/SimpleMatlabExample.cxa.rb
@@ -0,0 +1,16 @@
+abort "Run 'source [MUSCLE_HOME]/etc/muscle.profile' before this script" if not ENV.has_key?('MUSCLE_HOME')
+dir = ENV['MUSCLE_HOME'] + '/share/muscle/examples/simplematlab'
+
+# declare kernels
+w = Instance.new('w', 'muscle.core.standalone.MatlabKernel')
+r = Instance.new('r', 'examples.simplejava.ConsoleWriter')
+
+# configure connection scheme
+w.couple(r, 'data', ['multiply_0.5'])
+
+# configure cxa properties
+$env['max_timesteps'] = 4
+$env['default_dt'] = 1;
+
+w['script'] = "#{dir}/simplematlab.m"
+
