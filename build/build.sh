22d657f557e458319ac5aa9719a394eb49be2ac1
Joris Borgdorff <joris@jorisborgdorff.nl>
2014-05-15 14:46:10 +0000

    [Build] Disposed of the multiple-installation options "maintenance mode"
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@1066 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function usage {
-	echo "USAGE: $0 [-h|--help|INSTALL_PREFIX] [install|maintenance]"
+	echo "USAGE: $0 [-h|--help|INSTALL_PREFIX]"
 	echo "Builds and installs MUSCLE to INSTALL_PREFIX (default: /opt/muscle)."
 	echo "If file \`hostname\`.conf is present, a RELEASE and DEBUG version are installed"
 	echo "in \$INSTALL_PREFIX/devel and \$INSTALL_PREFIX/devel-debug, respectively."
 	exit $1
 }
 
-if [[ $# -eq 1 && ($1 = "--help" || $1 = "-h") ]]; then
+if [ $# -eq 1 ] && { [ "$1" = "--help" ] || [ "$1" = "-h" ];}; then
 	usage 0
 fi
 
@@ -20,7 +20,6 @@ cd build
 
 #2. Source local configuration
 HOSTNAME=`hostname -f`
-MODE="install"
 if [ -f "$HOSTNAME.conf" ]; then
 	. "$HOSTNAME.conf"
 	echo "Using configuration in $HOSTNAME.conf"
@@ -34,18 +33,10 @@ else
 	echo "No preset configuration for $HOSTNAME is present; performing regular installation"
 fi
 
-if [ $# -ge 1 ]; then
-	if [ "$1" = "install" ]; then
-		MODE="install"
-	elif [ "$1" = "maintenance" ]; then
-		MODE="maintenance"
-	else
-		# If given, always use install prefix from argument
-		INSTALL_PREFIX="$1"
-		if [ $# -eq 2 ]; then
-			MODE="$2"
-		fi
-	fi
+if [ $# -ge 1 ] && [ "$1" != "install" ] && [ "$1" != "maintenance" ]
+then
+	# If given, always use install prefix from argument
+	INSTALL_PREFIX="$1"
 fi
 if [ "$INSTALL_PREFIX" = "" ]; then
 	# If no INSTALL_PREFIX is set yet, use the default
@@ -71,10 +62,10 @@ else
 			usage 1
 		fi
 	fi
-
 fi
+
 INSTALL_DIR=`dirname "$INSTALL_PREFIX"`
-if [[ -w "$INSTALL_DIR" || -w $INSTALL_PREFIX ]]; then
+if [ -w "$INSTALL_DIR" ] || [ -w "$INSTALL_PREFIX" ]; then
 	echo "Installing MUSCLE in $INSTALL_PREFIX"
 else
 	echo "Can not install MUSCLE in $INSTALL_PREFIX: permission denied; try"
@@ -83,37 +74,14 @@ else
 	usage 1
 fi
 
-if [ "$MODE" = "install" ]; then
-	#3. Install MUSCLE
-	echo "========== BUILDING MUSCLE ==========="
-	cmake -DMUSCLE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release $MUSCLE_CMAKE_OPTIONS .. \
-	&& make clean && (make -j 4; make install)
-	if [ $? -eq 0 ]; then
-		echo "----------- MUSCLE INSTALLED IN $INSTALL_PREFIX -----------"
-	else
-		echo "!!!!!!!!!! BUILDING MUSCLE FAILED !!!!!!!!!!!"
-		exit 1
-	fi
+#3. Install MUSCLE
+echo "========== BUILDING MUSCLE ==========="
+cmake -DMUSCLE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release $MUSCLE_CMAKE_OPTIONS .. \
+&& make clean && (make -j 4; make install)
+if [ $? -eq 0 ]; then
+	echo "----------- MUSCLE INSTALLED IN $INSTALL_PREFIX -----------"
 else
-	#3. Build release
-	echo "========== BUILDING RELEASE ==========="
-	cmake -DMUSCLE_INSTALL_PREFIX=$INSTALL_PREFIX/devel -DCMAKE_BUILD_TYPE=Release $MUSCLE_CMAKE_OPTIONS .. \
-	&& make clean && (make -j 4; make install)
-	if [ $? -eq 0 ]; then
-		echo "----------- RELEASE INSTALLED IN $INSTALL_PREFIX/devel -----------"
-	else
-		echo "!!!!!!!!!! BUILDING RELEASE FAILED !!!!!!!!!!!"
-		exit 1
-	fi
-	
-	#4. Build with debug symbols
-	echo "========== BUILDING DEBUG version ==========="
-	cmake -DMUSCLE_INSTALL_PREFIX=$INSTALL_PREFIX/devel-debug -DCMAKE_BUILD_TYPE=Debug $MUSCLE_CMAKE_OPTIONS .. \
-	&& make clean && (make -j 4; make install)
-	if [ $? -eq 0 ]; then
-		echo "----------- DEBUG INSTALLED IN $INSTALL_PREFIX/devel-debug -----------"
-	else
-		echo "!!!!!!!!!! BUILDING DEBUG FAILED !!!!!!!!!!!"
-		exit 1
-	fi
+	echo "!!!!!!!!!! BUILDING MUSCLE FAILED !!!!!!!!!!!"
+	exit 1
 fi
+
