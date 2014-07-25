b93f09b4fab372af6a89bc3ddc2a13f54b6a1930
Joris Borgdorff <joris@jorisborgdorff.nl>
2014-07-25 14:49:51 +0000

    [C++] Added stacktrace function
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@1069 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- src/cpp/muscle2/util/exception.hpp
+++ src/cpp/muscle2/util/exception.hpp
@@ -28,6 +28,9 @@
 #include <string>  // std::string
 #include <errno.h> // errno
 #include <cstring> // strerror
+#include <cstdlib>
+#include <execinfo.h>
+#include <cxxabi.h>
 
 namespace muscle {
 
@@ -53,7 +56,92 @@ public:
 		else
 			logger::severe(w);
 	}
-    muscle_exception(std::string msg, int code, bool silent) throw() : std::runtime_error(msg), error_code(code) { if (!silent) log(); }
+    muscle_exception(std::string msg, int code, bool silent) throw() : std::runtime_error(msg), error_code(code) { if (!silent) { log(); print_stacktrace(); } }
+	
+	// stacktrace.h (c) 2008, Timo Bingmann from http://idlebox.net/
+	// published under the WTFPL v2.0
+
+	/** Print a demangled stack backtrace of the caller function to FILE* out. */
+	static void print_stacktrace(unsigned int max_frames = 63)
+	{
+		size_t len;
+		char *str = new char[max_frames*512];
+		len = sprintf(str, "stack trace:\n");
+		
+		// storage array for stack trace address data
+		void* addrlist[max_frames+1];
+		
+		// retrieve current stack addresses
+		int addrlen = backtrace(addrlist, (int)(sizeof(addrlist) / sizeof(void*)));
+		
+		if (addrlen == 0) {
+			len += sprintf(str+len, "  <empty, possibly corrupt>\n");
+			logger::severe(str);
+			delete [] str;
+			return;
+		}
+		
+		// resolve addresses into strings containing "filename(function+address)",
+		// this array must be free()-ed
+		char** symbollist = backtrace_symbols(addrlist, addrlen);
+		
+		// allocate string which will be filled with the demangled function name
+		size_t funcnamesize = 256;
+		char* funcname = (char*)malloc(funcnamesize);
+		
+		// iterate over the returned symbol lines. skip the first, it is the
+		// address of this function.
+		for (int i = 1; i < addrlen; i++) {
+			char *begin_name = 0, *begin_offset = 0, *end_offset = 0;
+			
+			// find parentheses and +address offset surrounding the mangled name:
+			// ./module(function+0x15c) [0x8048a6d]
+			for (char *p = symbollist[i]; *p; ++p) {
+				if (*p == '(')
+					begin_name = p;
+				else if (*p == '+')
+					begin_offset = p;
+				else if (*p == ')' && begin_offset) {
+					end_offset = p;
+					break;
+				}
+			}
+			
+			if (begin_name && begin_offset && end_offset
+				&& begin_name < begin_offset) {
+				*begin_name++ = '\0';
+				*begin_offset++ = '\0';
+				*end_offset = '\0';
+				
+				// mangled name is now in [begin_name, begin_offset) and caller
+				// offset in [begin_offset, end_offset). now apply
+				// __cxa_demangle():
+				
+				int status;
+				char* ret = abi::__cxa_demangle(begin_name,
+												funcname, &funcnamesize, &status);
+				if (status == 0) {
+					funcname = ret; // use possibly realloc()-ed string
+					len += sprintf(str+len, "  %s : %s+%s\n",
+							symbollist[i], funcname, begin_offset);
+				} else {
+					// demangling failed. Output function name as a C function with
+					// no arguments.
+					len += sprintf(str+len, "  %s : %s()+%s\n",
+							symbollist[i], begin_name, begin_offset);
+				}
+			} else {
+				// couldn't parse the line? print the whole line.
+				len += sprintf(str+len, "  %s\n", symbollist[i]);
+			}
+		}
+		
+		free(funcname);
+		free(symbollist);
+		
+		logger::severe(str);
+		delete [] str;
+	}
 };
 
 }
