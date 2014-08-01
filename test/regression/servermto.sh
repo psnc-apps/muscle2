77396f602dbc648a132b0f06ca8d11ca76acca46
Joris Borgdorff <joris@jorisborgdorff.nl>
2014-08-01 09:37:00 +0000

    [MTO] Do not throw exception with malformed header error, simply disconnect from the misbehaving process
    git-svn-id: https://apps.man.poznan.pl/svn/muscle/muscle/trunk@1070 b725f6ce-2418-43e8-b868-1e0ad3d2b257

--- test/regression/servermto.sh
+++ test/regression/servermto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export MUSCLE_HOME=/usr/local
-mto --topology mto-topology.cfg --config mto-config-$1.cfg --logFile server-$1.log --debug --MPWPath --logLevel DEBUG
+mto --topology mto-topology.cfg --config mto-config-$1.cfg --logFile server-$1.log --debug --MPWPath --logLevel TRACE --channels 64
